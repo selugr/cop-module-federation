5a059dfffd29862bc883b7376c21b39596c3bd0b
José Luis González Ruiz <jose.gonzalez.sge@mango.com>
2021-10-28 23:44:24 +0200

    init

---
+++ src/App.test.js
@@ -0,0 +1,12 @@
+import { render, screen, waitFor } from '@testing-library/react'
+import App from './App'
+
+test( 'renders title', async () => {
+    // act(() => {
+    render( <App /> )
+    const title = screen.getByText( /The Phone Shop/i )
+    await waitFor( () => {
+        expect( title ).toBeInTheDocument()
+    } )
+    // })
+} )
