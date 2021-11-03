19de1dd4583770a7d2bde73f91c2112358b259d1
Uri Ustrell <oriol.ustrell.sge@mango.com>
2021-11-03 12:42:41 +0100

    fix: publicPath auto

--- federated/components/shared-ui/config/webpack.common.js
+++ federated/components/shared-ui/config/webpack.common.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, '../dist'),
     filename: 'main.js',
-    publicPath: '/',
+    publicPath: 'auto',
     library: {
       type: 'commonjs2'
     }
